0dc2b535793b9010c8b75c49c950b3e5efe51772
0xjrx <mmarquard737@gmail.com>
2025-02-24 21:54:12 +0100

    feat: add Haiku page and link
    Refs: OPS-1337

--- senju/main.py
+++ senju/main.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
-    context = {
-        "number": 1337
-    }
-    return render_template("index.jinja", context=context, title="Senju")
+    return render_template("index.jinja", title="Senju", active_page="home")
+
+@app.route("/haiku")
+def haiku_page():
+    return render_template("haiku.jinja", title="Haiku of the Day", active_page="haiku")
+
+
+
