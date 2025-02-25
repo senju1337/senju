ce42cb4eea39a98c01554f12498ef87998261790
0xjrx <mmarquard737@gmail.com>
2025-02-25 14:54:24 +0100

    fix: remove dictionary and add uniform view names
    Refs: OPS-11

--- senju/main.py
+++ senju/main.py
@@ -2,14 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-urls = {
-        "home": "index_view",
-        "haiku":"haiku_view"
-    }
+
 
 @app.route("/")
 def index_view():
-    return render_template("index.jinja", title="Senju", urls=urls)
+    return render_template("index.jinja", title="Senju")
 
 
 @app.route("/haiku")
@@ -17,5 +14,4 @@ def haiku_view():
 
     return render_template(
         "haiku.jinja",
-        title="Haiku of the Day",
-        urls = urls)
+        title="Haiku of the Day")
