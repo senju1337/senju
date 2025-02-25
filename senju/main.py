44ad774ada0905857bbd20d675cba77b1ff7f6e9
Christoph J. Scherr <software@cscherr.de>
2025-02-25 18:05:16 +0100

    feat: redirect /haiku to /haiku/<latest-id>
    Refs: OPS-25

--- senju/main.py
+++ senju/main.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from logging import Logger
+from pathlib import Path
+from flask import Flask, redirect, render_template, url_for
+
+from senju.haiku import Haiku
+from senju.store_manager import StoreManager
 
 app = Flask(__name__)
+
+store = StoreManager(Path("/tmp/store.db"))
+
+testh = Haiku("hello world this is a haiku (not)")
+some_uuid = store.save_haiku(testh)
 
 
 @app.route("/")
@@ -8,9 +18,28 @@ def index_view():
     return render_template("index.html", title="Senju")
 
 
-@app.route("/haiku")
-def haiku_view():
+@app.route("/haiku/")
+def haiku_index_view():
+    haiku_id: int | None = store.get_id_of_latest_haiku()
+    if haiku_id is None:
+        # TODO: add "empty haiku list" error page
+        raise KeyError("no haiku exist yet")
+    return redirect(url_for("haiku_view", haiku_id=haiku_id))
+
+
+@app.route("/haiku/<int:haiku_id>")
+def haiku_view(haiku_id):
+    logger = Logger(__name__)
+    logger.debug(haiku_id)
+    haiku: Haiku | None = store.load_haiku(haiku_id)
+    if haiku is None:
+        # TODO: add "haiku not found" page
+        raise KeyError("haiku not found")
+    context: dict = {
+        "haiku": haiku
+    }
 
     return render_template(
-        "haiku.html",
+        "haiku.jinja",
+        context=context,
         title="Haiku of the Day")
