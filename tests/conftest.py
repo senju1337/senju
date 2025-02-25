5aba3981ee42a74f7ee2f0154e2e09ab988f8e49
Christoph J. Scherr <software@cscherr.de>
2025-02-25 15:21:27 +0100

    test: store_manager sanity check
    Refs: OPS.19

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,15 @@ from pathlib import Path
 
 import pytest
 
+from senju.store_manager import StoreManager
+
 
 @pytest.fixture(scope="session")
 def temp_data_dir():
     """Create a temporary directory for test data"""
     return Path(tempfile.mkdtemp())
+
+
+@pytest.fixture(scope="session")
+def store_manager(temp_data_dir):
+    return StoreManager(temp_data_dir/"store.db")
