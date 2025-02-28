7d41df6976703a2b676b2dc5ffa4795e1e1304cc
Christoph J. Scherr <software@cscherr.de>
2025-02-28 16:14:23 +0100

    fix: containerized senju did not start properly
    Refs: OPS-10

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 curl http://ollama:11434/api/pull -d '{"model": "llama3.2:1b"}'
 
-flask --app senju/main run --debug --host=0.0.0.0
-
+cd /app
+poetry run sh -c 'flask --app senju/main run --host=0.0.0.0'
